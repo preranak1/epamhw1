5d42621ef502091d4ed8159e3c3fca7da304f74a
preranak1 <preranakomravelly@gmail.com>
2020-09-23 15:59:52 +0530

    "First HW of EPAM"

---
+++ java1/src/java1/java.java
@@ -0,0 +1,13 @@
+package java1;
+
+public class java {
+	public static void main(String args[]) {
+	int number1=5;
+	double number2=6;
+	int num=(int)number2;
+	int sum;
+	sum= num+number1;
+	System.out.println(sum);
+	
+	}
+}
